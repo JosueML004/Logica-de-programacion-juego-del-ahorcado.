8afcb96162a9540311446d7f7bceac6f351af311
JosueML004 <josuemendozalop@gmail.com>
2024-06-30 18:01:34 -0500

    Add files via upload

---
+++ DiccionarioPython.py
@@ -0,0 +1,4 @@
+#LibreriaPython
+palabras_faciles = ["sol", "luna", "flor","casa", "feo", "raton", "choza"]
+palabras_medias = ["guitarra", "ventana", "libro", "perro", "gato"]
+palabras_dificiles = ["computadora", "elefante", "universidad", "telescopio", "astronomia"]
